98a8dbfe7c0f800e1523503d46de7cfdae9c98ee
Phil Picton <phil@dropletmedia.co.uk>
2021-08-06 17:39:17 +0100

    get tailwind working, fix console errors from babel

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,26 +6,22 @@ export default {
   head: {
     title: 'experiment',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: ['@/css/styles.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -51,5 +47,29 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+    postcss: {
+      // Add plugin names as key and arguments as value
+      // Install them before as dependencies with npm or yarn
+      plugins: {
+        // Disable a plugin by passing false as value
+        'postcss-nested': {},
+        'autoprefixer': {},
+      },
+      // THIS FIXES AN ANNOYING ERROR IN THE CONSOLE!!
+      babel: {
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              targets: {
+                esmodules: true,
+              },
+              loose: true,
+              shippedProposals: true,
+            },
+          ],
+        ],
+      },
+    },
+  },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="prose">
-<h1>Hello world</h1>
-
+  <div class="container">
+    <h1>Hello world</h1>
   </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style scoped lang="postcss">
+.container {
+  & h1 {
+    font-size: 40px;
+  }
+  @apply inline-block bg-gray-200 rounded-full px-3 py-1 my-5 text-sm font-semibold text-gray-700;
+  &:hover {
+    @apply bg-gray-300;
+  }
+}
+</style>
