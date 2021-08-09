f6ea89521063f6567fb28a05e0c634298b18bb4c
Phil Picton <phil@dropletmedia.co.uk>
2021-08-09 15:50:28 +0100

    add code style config

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,23 @@ module.exports = {
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'comma-dangle': [2, 'always-multiline'],
+    curly: [2, 'multi-line'],
+    'max-len': 'off',
+    'space-before-function-paren': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
+    'vue/no-v-html': 0,
+  },
 }
